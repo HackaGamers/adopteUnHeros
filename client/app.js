9fe6d7b407948d4453f4a4aafc6e8bdc29a837eb
Capucine Leclerc <capucine_49@hotmail.com>
2018-04-05 20:10:21 +0200

    app. js recuperation json

---
+++ client/app.js
@@ -0,0 +1,19 @@
+
+ const profileHero = document.getElementById('heros')
+
+
+		fetch('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/234.json')
+		.then(res => res.json())
+
+
+		.then(resJson => {
+			
+			profileHero.innerHTML =`<div class="profile">
+			<p> je m'appelle ${resJson.name} et je suis un  ${resJson.appearance.gender}.
+			Je suis un ${resJson.appearance.race}, je mesure ${resJson.appearance.height[1]}.
+			Je pèse ${resJson.appearance.weight[1]}. Je suis un peu gros mais confortable. 
+			J'ai les yeux ${resJson.appearance.eyeColor}, et les cheveux ${resJson.appearance.hairColor}. Mon travail est 	${resJson.work.occupation}. J'ai fait ma première apparition dans  ${resJson.biography.firstAppearance};
+			Voici ma photo :p </p>
+			<img src = "${resJson.images.sm}"></div>`
+
+		})
